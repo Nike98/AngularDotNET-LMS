1dc94df51d0a32735cc5c75a5fb2dfc896e7b8e3
Nike98 <nikhilagarwal20@yahoo.com>
2024-01-22 01:13:36 +0530

    Application Testing - Added new Controller in API Project, made changes to UI project, added shared module with button common module, added new Page Header module,

---
+++ UI/src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+
+
+@NgModule({
+  declarations: [
+    PageHeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [ CommonModule, MaterialModule, PageHeaderComponent ],
+})
+export class SharedModule { }
