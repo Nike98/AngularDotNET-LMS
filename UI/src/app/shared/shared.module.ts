a41a5ede30566abc4fb951d56b7e011d7e9cf53b
Nike98 <nikhilagarwal20@yahoo.com>
2024-02-04 21:07:30 +0530

    Added code to the previously added new components - Page Header, Footer, Side Nav.

--- UI/src/app/shared/shared.module.ts
+++ UI/src/app/shared/shared.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
-
+import { PageFooterComponent } from './components/page-footer/page-footer.component';
+import { PageSideNavComponent } from './components/page-side-nav/page-side-nav.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    PageHeaderComponent
+    PageHeaderComponent,
+    PageFooterComponent,
+    PageSideNavComponent,
+    PageNotFoundComponent,
   ],
-  imports: [
-    CommonModule
+  imports: [CommonModule, MaterialModule, RouterModule, ReactiveFormsModule],
+  exports: [
+    CommonModule,
+    MaterialModule,
+    PageHeaderComponent,
+    PageFooterComponent,
+    PageSideNavComponent,
+    RouterModule,
+    PageNotFoundComponent,
+    ReactiveFormsModule,
   ],
-  exports: [ CommonModule, MaterialModule, PageHeaderComponent ],
 })
-export class SharedModule { }
+export class SharedModule {}
