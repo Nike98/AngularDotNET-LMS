2192bf2a8979af0462dc2c748027deadfdf5a183
Nike98 <nikhilagarwal20@yahoo.com>
2024-02-06 22:15:52 +0530

    Added more HTML design and base validation logic to Register component

--- UI/src/app/auth/register/register.component.ts
+++ UI/src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'register',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  hidePswdContent: boolean = true;
+  hideCPswdContent: boolean = true;
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = fb.group({
+      firstName: fb.control('', [Validators.required]),
+      lastName: fb.control('', [Validators.required]),
+      email: fb.control('', [Validators.required]),
+      mobileNumber: fb.control('', [Validators.required]),
+      password: fb.control('', [Validators.required]),
+      cpassword: fb.control('', [Validators.required]),
+    });
+  }
+
+  register() {
+    let user = {
+      firstName: this.registerForm.get('firstName')?.value,
+      lastName: this.registerForm.get('lastName')?.value,
+      email: this.registerForm.get('email')?.value,
+      mobileNumber: this.registerForm.get('mobileNumber')?.value,
+      password: this.registerForm.get('password')?.value,
+    };
+  }
 }
