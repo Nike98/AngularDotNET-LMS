119028e6d9b7e2f304af51770976ee1886b31071
Nike98 <nikhilagarwal20@yahoo.com>
2025-01-17 00:33:39 +0530

    Removed Weather file and added new Model, Enum & context file

---
+++ API/API/Context.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Context
+    {
+    }
+}
---
+++ API/API/Entity/User.cs
@@ -0,0 +1,49 @@
+﻿namespace API.Entity
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string MobileNumber { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; }
+        public UserType UserType { get; set; } = UserType.NONE;
+        public AccountStatus AccountStatus { get; set; } = AccountStatus.UNAPPROVED;
+    }
+
+    public enum UserType { NONE, ADMIN, STUDENT }
+
+    public enum  AccountStatus { UNAPPROVED, ACTIVE, BLOCKED }
+
+    public class BookCategory
+    {
+        public int ID { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public string SubCategory { get; set; } = string.Empty;
+    }
+
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public float Price { get; set; }
+        public bool Ordered { get; set; }
+        public BookCategory? BookCategoryId { get; set; }
+    }
+
+    public class Order
+    {
+        public int ID { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public bool Returned { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public int FinePaid { get; set; }
+        public User? User { get; set; }
+        public Book? Book { get; set; }
+    }
+}
