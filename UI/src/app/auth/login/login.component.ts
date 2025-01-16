a4d0af8f796b2f9cf8d0747e563a7c9a36525180
Nike98 <nikhilagarwal20@yahoo.com>
2025-01-17 00:32:55 +0530

    Added login page to UI

--- UI/src/app/auth/login/login.component.ts
+++ UI/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'login',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  hidePswdContent: boolean = true;
 
+  constructor(fb: FormBuilder) {
+    this.loginForm = fb.group({
+      email: fb.control('', [Validators.required]),
+      password: fb.control('', [Validators.required]),
+    });
+  }
+
+  login () {
+    let loginInfo = {
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value,
+    }
+  }
 }
