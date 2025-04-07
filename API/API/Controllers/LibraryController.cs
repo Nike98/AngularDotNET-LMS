90492dc92ceb4d1bcffbb0f7b0c561baece07fd9
Nike98 <nikhilagarwal20@yahoo.com>
2025-04-08 01:21:24 +0530

    Added methods to handle Login & Register actions also added JWT Token handling for initial retreival

--- API/API/Controllers/LibraryController.cs
+++ API/API/Controllers/LibraryController.cs
@@ -1,6 +1,7 @@
 ﻿using API.Entity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 
 namespace API.Controllers
 {
@@ -19,7 +20,7 @@ namespace API.Controllers
         public EmailService EmailService { get; }
         public JwtService JwtService { get; }
 
-        [HttpPost]
+        [HttpPost("Register")]
         public ActionResult Register(User user)
         {
             user.AccountStatus = AccountStatus.ACTIVE;
@@ -29,9 +30,41 @@ namespace API.Controllers
             Context.Users.Add(user);
             Context.SaveChanges();
 
+            const string subject = "Account Created";
+            var body = $"""
+                <html>
+                    <body>
+                        <h1>Hello, {user.FirstName} {user.LastName} </h1>
+                        <h2>
+                            Yor account has been creaetd and we have sent approval request to admin.
+                            once the request is approved by admin you will receive email, and you will be
+                            able to login in to your account.
+                        </h2>
+                        <h3>Thanks</h3>
+                    </body>
+                </html>
+                """;
+
+            EmailService.SendEmail(user.Email, subject, body);
+
             return Ok(@"Thank you for registering.
                         Your Account has been sent for approval.
                         Once it is Approved, you will get an email.");
+        }
+
+        [HttpGet("Login")]
+        public ActionResult Login(string email, string password)
+        {
+            if (Context.Users.Any(u => u.Email.Equals(email) && u.Password.Equals(password)))
+            {
+                var user = Context.Users.Single(user => user.Email.Equals(email) && user.Password.Equals(password));
+
+                if (user.AccountStatus == AccountStatus.UNAPPROVED)
+                    return Ok("unapproved");
+
+                return Ok(JwtService.GenerateToken(user));
+            }
+            return Ok("not found");
         }
     }
 }
--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,5 +1,8 @@
 using API;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,30 @@ builder.Services.AddDbContext<Context>(o =>
 {
     o.UseSqlServer(builder.Configuration.GetConnectionString("DB"));
 });
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("LibraryPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+builder.Services.AddScoped<EmailService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = "localhost",
+        ValidAudience = "localhost",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+builder.Services.AddScoped<JwtService>();
 
 var app = builder.Build();
 
@@ -24,6 +51,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("LibraryPolicy");
 
 app.UseAuthorization();
 
